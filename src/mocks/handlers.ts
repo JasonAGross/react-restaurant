6576300839fa94c02f165f98c2783f7f1d66e11a
Jason Gross <jasongross86@gmail.com>
2022-09-29 11:22:04 -0400

    fork start options

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,3 +1,11 @@
 import { rest } from 'msw';
+import { mockFoods } from './mockFoods'
 
-export const handlers = [];
+export const handlers = [
+  rest.get('http://localhost:3001/foods', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json(mockFoods)
+    )
+  })
+];
---
+++ src/mocks/mockFoods.ts
@@ -0,0 +1,27 @@
+import { Food } from '../food'
+export const mockFoods: Food[] = [
+  {
+    "id": 1,
+    "name": "Burger",
+    "image": "burger.jpg",
+    "price": 8.99,
+    "description": "This ain't your average burger. Topped with our tangy cheddar cheese sauce, fresh lettuce, and tomato.",
+    "tags": ["Lunch", "Dinner"]
+  },
+  {
+    "id": 2,
+    "name": "Banana Blueberry French Toast",
+    "image": "banana-french-toast.jpg",
+    "price": 9.99,
+    "description": "Delicious french toast with banana and blueberry.",
+    "tags": ["Breakfast"]
+  },
+  {
+    "id": 3,
+    "name": "Cajun Pasta",
+    "image": "cajun-pasta.jpg",
+    "price": 16.99,
+    "description": "Creole-style pasta that's guaranteed to make you sweat.",
+    "tags": ["Lunch", "Dinner", "Spicy"]
+  },
+]
